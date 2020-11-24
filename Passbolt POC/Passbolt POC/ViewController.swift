5a2967456e2162e183bc3db8a557ac1dc2ce4080
Wojciech Piwoński <wojciech.piwonski@polidea.com>
2020-11-24 16:37:58 +0100

    Storyboards version of the app

---
+++ Passbolt POC/Passbolt POC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Passbolt POC
+//
+//  Created by Wojciech Piwoński on 11/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
